b2e08dadbb65c06843a42c534f9aa954cdb349fb
bruntrd <bruntrd@gmail.com>
2015-08-03 21:43:15 -0500

    added functionality/styling

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,40 +7,43 @@ $(document).ready(function(){
 
             $.each(data,function(){
                 $(".content").append("<div class='newStudent'><div>");
-                var el = $('.newStudent').children().last()
-                el.append("<p>Name: " + this.name +"</p>");
-                el.append("<p>Elevator Pitch: " + this.desc +"</p>");
-                el.append("<p>Shout out: " +this.thanks +"</p>");
+                var el = $('.newStudent').children().last();
+                el.append("<p><em>" + this.name +"</em></p>");
+                el.append("<p><strong>Personal Description</strong>: " + this.desc +"</p>");
+                el.append("<p><strong>Shout out</strong>: " +this.thanks +"</p>");
 
             });
             $('.newStudent:first').addClass('activeStudent');
 
+
         }
     })
 
-    $("#rightArrow").on('click',function(){
+    $('#rightArrow').on('click',function(){
         var currentSlide = $('.activeStudent');
         var nextSlide = currentSlide.next();
 
-        //if (nextSlide.length === 0){
-        //    nextSlide = $('.newStudent:first');
-        //}
+        if (nextSlide.length === 0){
+            nextSlide = $('.newStudent:first');
+        }
+
 
         currentSlide.fadeOut(500).removeClass('activeStudent');
         nextSlide.fadeIn(500).addClass('activeStudent');
-
     });
+
 
     $("#leftArrow").on('click',function(){
         var currentSlide = $('.activeStudent');
-        var nextSlide = currentSlide.last();
+        var nextSlide = currentSlide.prev();
 
-        //if (nextSlide.length ===0){
-        //    nextSlide = $('.newStudent:first');
-        //}
+        if (nextSlide.length ===0){
+            nextSlide = $('.newStudent:last');
+        }
+
 
         currentSlide.fadeOut(500).removeClass('activeStudent');
-        nextSlide.fadeIn(500).addClass('activeSudent');
+        nextSlide.fadeIn(500).addClass('activeStudent');
     });
 
 });
